3a8bf659b57e20d30346fd2c7243089715287890
vishalitmail <vishalitmail@gmail.com>
2023-04-22 12:14:48 +0530

    pig

--- minor-projects/Java-script/Pig-Game/script.js
+++ minor-projects/Java-script/Pig-Game/script.js
@@ -80,7 +80,7 @@ btnHold.addEventListener('click', function () {
         //scores[1]=scores[1]+currentScore;
 
         //2.check if player's score is >=100
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= 100) {
             //finish the game
             playing = false;
             diceEl.classList.add('hidden');
