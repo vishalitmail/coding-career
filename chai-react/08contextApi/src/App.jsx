5dee606c3b9d6509898cdcea9cfb43cccdec07e3
vishalitmail <vishalitmail@gmail.com>
2024-02-01 08:53:51 +0530

    08contextApi revison

---
+++ chai-react/08contextApi/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+function App() {
+
+
+  return (
+
+    <UserContextProvider>
+      <h1>chai aur code</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+
+
+  )
+}
+
+export default App
